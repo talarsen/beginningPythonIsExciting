ffc20428654d4d70f3da3600e0134c04ef336994
Tara Larsen <talarsen74@gmail.com>
2022-11-11 13:54:43 -0800

    Create largestNumInList.py

---
+++ largestNumInList.py
@@ -0,0 +1,9 @@
+# find the largest number
+
+
+largest_number_so_far = -1
+numberList = [2,5,7,88,4]
+for num in numberList:
+  if num > largest_number_so_far :
+    largest_number_so_far = num
+print(largest_number_so_far)
